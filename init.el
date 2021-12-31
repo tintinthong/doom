0bade60b547b420c9d920da8d11c592d4eb8fb74
Justin <justinthong93@gmail.com>
2022-01-01 00:44:06 +0800

    remove rss

--- init.el
+++ init.el
@@ -174,9 +174,9 @@
        ;;(wanderlust +gmail)
 
        :app
-       ;;calendar
+       calendar
        irc               ; how neckbeards socialize
-       ;;(rss +org)        ; emacs as an RSS reader
+       ;; (rss +org)        ; emacs as an RSS reader
        twitter           ; twitter client https://twitter.com/vnought
 
        :config
